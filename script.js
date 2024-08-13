03a63fb4fdb58387ab97d057069de494be3995de
adryano10 <adryano.sist@outlook.com>
2024-08-12 23:10:07 -0300

    Primeiro Commit projeto responsividade

---
+++ script.js
@@ -0,0 +1,9 @@
+let form = document.querySelector(".Formulario");
+
+function mostarFormulario() {
+  form.style.bottom = "-15%";
+}
+
+function fecharFormulario() {
+  form.style.bottom = "-100%";
+}
